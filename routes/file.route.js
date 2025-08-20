7b7bfbf613cbf73748815cbfeb32397ee572294c
Raphael Ragoomundun <rragoomundun@protonmail.com>
2025-08-20 21:01:43 +0400

    Delete file

--- routes/file.route.js
+++ routes/file.route.js
@@ -2,14 +2,16 @@ import express from 'express';
 
 import { upload } from '../files/upload.js';
 
-import { uploadFile } from '../controllers/file.controller.js';
+import { deleteFile, uploadFile } from '../controllers/file.controller.js';
 
-import { fileUploadValidator } from '../validators/file.validator.js';
+import { fileUploadValidator, fileDeleteValidator } from '../validators/file.validator.js';
 
 import authenticateMiddleware from '../middlewares/authenticate.middleware.js';
 
 const router = express.Router();
 
-router.post('/', authenticateMiddleware, upload.single('file'), fileUploadValidator, uploadFile);
+router
+  .post('/', authenticateMiddleware, upload.single('file'), fileUploadValidator, uploadFile)
+  .delete('/', authenticateMiddleware, fileDeleteValidator, deleteFile);
 
 export default router;
